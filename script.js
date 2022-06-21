64dd1e5e9c1d33a9008d08cd4e5ed4b9c6260023
Onyiana <adaurecananaba@gmail.com>
2022-06-21 20:58:57 +0100

    java

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector(".myHeight").textContent = "4.7feet"
+document.querySelector(".myName").textContent = "Onyinyechi Ananaba"
+document.querySelector(".myCountry").textContent = "Nigeria"
